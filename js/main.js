989932b1740f70ea13b16e6dfb22d0934f23639d
carmenpmoreno <carmenp.moreno@gmail.com>
2019-07-05 14:29:27 +0200

    first interaction with js

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const buttonOpen = document.querySelector('.nav-trigger');
+const buttonClose = document.querySelector('.nav-close');
+const navContainer = document.querySelector('.nav');
+const pageContainer = document.querySelector('.page');
+
+buttonOpen.addEventListener('click', handleButtonOpenClick);
+
+function handleButtonOpenClick(event) {
+    navContainer.classList.add('nav__opened');
+    navContainer.classList.remove('nav');
+    pageContainer.classList.add('page__menu-opened')
+    buttonOpen.classList.add('hidden');
+}
+
+buttonClose.addEventListener('click', handleButtonCLoseClick);
+
+function handleButtonCLoseClick(event) {
+    navContainer.classList.remove('nav__opened');
+    navContainer.classList.add('nav');
+    pageContainer.classList.remove('page__menu-opened');
+    buttonOpen.classList.remove('hidden');
+}
